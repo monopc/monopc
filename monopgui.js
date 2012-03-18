7994af98862756e9fca292e56bafaa879c6aff51
funatiker <koehlerx@gmx.de>
2012-03-18 16:29:59 +0000

    Monopgui is now a real Object

--- monopgui.js
+++ monopgui.js
@@ -1,13 +1,18 @@
-var monopgui = {
-	chatbox: document.getElementById("chatbox"),
-	errorbox: document.getElementById("errorbox"),
-	gamelist: document.getElementById("gamelist"),
-	gamelistlines: [],
-	deletegame: function (id){
+var monopgui = new Monopgui();
+function Monogui(){
+	if ( !(this instanceof arguments.callee) ) {
+		return new Monogui();
+	}
+	
+	this.chatbox = document.getElementById("chatbox");
+	this.errorbox = document.getElementById("errorbox");
+	this.gamelist = document.getElementById("gamelist");
+	this.gamelistlines = [];
+	this.deletegame = function (id){
 		this.gamelistlines[id].parentNode.removeChild(this.gamelistlines[id]);
 		this.gamelistlines.splice(id, 1);
-	},
-	updategame: function (id){
+	};
+	this.updategame = function (id){
 		//Find out whether the <li> already exists
 		if (this.gamelistlines[id] !== undefined){
 			//the <li> exists
@@ -20,13 +25,13 @@ var monopgui = {
 		//" (" + monopc.gametypes[monopc.games[id].gametype]["name"] + " " +
 		" (" +
 		monopc.games[id].players + " Players) - " + monopc.games[id].description;
-	},
-	printmessage: function(type, playerid, author, value){
+	};
+	this.printmessage = function(type, playerid, author, value){
 		//TODO: write something
 		if (type == "chat"){
 			this.chatbox.innerHTML = "<p>&lt;" + author +  "&gt; " + value + "</p>" + this.chatbox.innerHTML;
 		}else{
 			this.errorbox.innerHTML = "<p><b>" + type +  "</b> " + value + "</p>" + this.errorbox.innerHTML;
 		}
-	}
-};
+	};
+}
